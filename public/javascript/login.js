04135f77e3b1d16e956cecf3d46e5a7d635317c7
Richard <richardyjkim@gmail.com>
2021-05-04 20:55:23 -0700

    feature/single-post completed

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,30 +1,3 @@
-async function signupFormHandler(event) {
-  event.preventDefault();
-
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        email,
-        password
-      }),
-      header: { 'Content-type': 'application/json' }
-    });
-
-    // check the response status
-    if (response.ok) {
-      console.log('success');
-    } else {
-      alert(response.statustText)
-    }
-  }
-}
-
 async function loginFormHandler(event) {
   event.preventDefault();
 
@@ -49,5 +22,32 @@ async function loginFormHandler(event) {
   }
 }
 
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
